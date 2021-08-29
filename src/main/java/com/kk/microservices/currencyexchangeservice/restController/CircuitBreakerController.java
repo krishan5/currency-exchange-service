91eda55abcf836fb29ddaf44ede5585b9ceb5930
Krishan Kumar <kktech970@gmail.com>
2021-08-29 19:30:46 +0530

    Resilience4j rate limiter and bulkhead

--- src/main/java/com/kk/microservices/currencyexchangeservice/restController/CircuitBreakerController.java
+++ src/main/java/com/kk/microservices/currencyexchangeservice/restController/CircuitBreakerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -45,5 +46,19 @@ public class CircuitBreakerController {
 		logger.info("Sample api 2 call recieved - CircuitBreaker");
 		ResponseEntity<String> response = new RestTemplate().getForEntity("http://localhost:8080/dummyurl", String.class);
 		return response.getBody();
+	}
+	
+	@GetMapping("/sample-api-3")
+	@RateLimiter(name = "sample-api-3")
+	public String sampleApi3() {
+		logger.info("Sample api 3 call recieved - RateLimiter");
+		return "sample-api-3";
+	}
+	
+	@GetMapping("/sample-api-4")
+	@RateLimiter(name = "sample-api-4")
+	public String sampleApi4() {
+		logger.info("Sample api 4 call recieved - RateLimiter");
+		return "sample-api-4";
 	}
 }
